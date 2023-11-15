c9eb0173f02f0be145c696ae8ac0ab3ee543abec
Your Name <you@example.com>
2023-11-15 14:15:09 +0300

    Routes Done

---
+++ index.js
@@ -0,0 +1,23 @@
+
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+let urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true })); //Для принятия данных из формы. Обязательно!!!
+app.use(bodyParser.json());//Для принятия данных из формы. Обязательно!!!
+
+let index = require('./routes/index');
+app.use('/', index);
+
+let article = require('./routes/article');
+app.use('/article', article);
+
+let admin = require('./routes/admin');
+app.use('/admin', admin);
+
+
+app.listen(3000);
+
