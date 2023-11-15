157c6dc34f44c2245bf0e59738ceb2c0eb83ac79
Your Name <you@example.com>
2023-11-15 15:17:53 +0300

    eror

--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,6 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 const jsonParser = express.json();
 
 //Контроллеры для админ панели
->>>>>>> 651bb2ae17f6353c4cd84d984417d19fad28ff46
 router.get('/',adminController.getArticleAll);
 router.get('/:idArticle',adminController.getArticle)
 router.post('/addArticle',adminController.addArticle);
